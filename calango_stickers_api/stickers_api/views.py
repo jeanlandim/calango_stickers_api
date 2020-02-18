09d716778e1f78669e1a33bd310ed7ad0b14f6e4
Jean Landim <jewanbb@gmail.com>
2020-02-18 09:32:41 -0300

    to be continued

--- calango_stickers_api/stickers_api/views.py
+++ calango_stickers_api/stickers_api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+
 from .models import StickersModels
 from .serializers  import StickersSerializer
 from .filters import StickersFilter
+
+from django.http import JsonResponse
+from jsonrpcserver import method, dispatch
 
 class StickersApiList(generics.ListCreateAPIView):
     queryset = StickersModels.objects.all()
@@ -16,3 +20,25 @@ class StickersApiListItems(generics.RetrieveUpdateDestroyAPIView):
 
 def stickers(request):
     return render(request,'index.html')
+
+# jsonrpcserver statements
+
+@method 
+def ls(): # list stickers available
+    stickers = []
+    for sticker in StickersModels.objects.values():
+        if sticker['sticker_quantity'] => 1:
+           stickers.append(sticker['sticker_name']) 
+    return(stickers)
+
+@method 
+def cat(such_sticker): # get description of stickers 
+    stickers_description = '' 
+    for sticker in StickersModels.objects.values():
+        if sticker['sticker_quantity'] => 1:
+           stickers_description = sticker['sticker_description'] 
+
+    return(stickers_description)
+
+
+    
